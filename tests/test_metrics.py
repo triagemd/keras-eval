46caa2c85f69b01e475c11f5fbfb762ef8f3643c
Albert Jimenez Sanfiz <albert@Alberts-MacBook-Pro.local>
2018-02-26 12:33:58 -0400

    fix test_metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -18,7 +18,7 @@ def test_accuracy_top_k():
     probs = np.asarray([[0.3, 0.7], [0.4, 0.6], [0.9, 0.1], [0.2, 0.8]])  # First sample an error.
     pred_acc = accuracy_top_k(probs, y_true, k=1)
     assert pred_acc == 0.75, "Accuracy should have 1/4 error."
-    pred_acc = accuracy_top_k(probs, y_true k=2)
+    pred_acc = accuracy_top_k(probs, y_true, k=2)
     assert pred_acc == 1, "Accuracy should have 0 error since k=2."
     # Add an extra class.
     probs = np.asarray([[0.1, 0.7, 0.2], [0.4, 0.6, 0], [0.9, 0.1, 0], [0.2, 0.8, 0]])  # First sample an error.
@@ -27,11 +27,11 @@ def test_accuracy_top_k():
 
 
 def test_metrics_top_k():
-    labels = ['class0', 'class1']
+    class_names = ['class0', 'class1']
     y_true = np.asarray([1, 1, 0])  # 3 samples, 2 classes.
     y_probs = np.asarray([[0, 1], [0.1, 0.9], [0.8, 0.2]])
 
-    met = metrics_top_k(y_probs, y_true, labels, k_vals=[1, 2], print_screen=False)
+    met = metrics_top_k(y_probs, y_true, class_names, k_vals=[1, 2], verbose=0)
     assert met[0]['sensitivity_k1'] == 1.0, 'sens should be 1 when k=1.'
     assert met[1]['sensitivity_k1'] == 1.0, 'sens should be 1 when k=1.'
     assert met[0]['precision_k1'] == 1.0, 'prec should be 1 when k=1.'
@@ -40,7 +40,7 @@ def test_metrics_top_k():
     assert met[1]['sensitivity_k1'] == 1.0, 'sens should be 1 when k=2.'
 
     y_probs = np.asarray([[1, 0], [0.1, 0.9], [0.8, 0.2]])  # First one is predicted incorrectly.
-    met = metrics_top_k(y_probs, y_true, labels, k_vals=[1, 2], print_screen=False)
+    met = metrics_top_k(y_probs, y_true, class_names, k_vals=[1, 2], verbose=0)
     assert met[0]['sensitivity_k1'] == 1.0, 'sens should be 1 when k=1, since all correct for class 0.'
     assert met[1]['sensitivity_k1'] == 0.5, 'sens should be 0.5 when k=1, since first is incorrect.'
     assert met[0]['precision_k1'] == 0.5, 'prec should be 0.5 when k=1, since predicts extra class 0.'
@@ -48,9 +48,9 @@ def test_metrics_top_k():
     assert met[0]['sensitivity_k2'] == 1.0, 'sens should be 1 when k=2.'
     assert met[1]['sensitivity_k2'] == 1.0, 'sens should be 1 when k=2.'
 
-    labels = ['class0', 'class1', 'class2']
+    class_names = ['class0', 'class1', 'class2']
     y_probs = np.asarray([[0.1, 0, 0.9], [0.1, 0.9, 0], [0.8, 0.2, 0]])  # Add extra class. First sample is incorrect.
-    met = metrics_top_k(y_probs, y_true, labels, k_vals=[1, 2], print_screen=False)
+    met = metrics_top_k(y_probs, y_true, class_names, k_vals=[1, 2], verbose=0)
     assert met[0]['sensitivity_k1'] == 1.0, 'sens should be 1 when k=1, since all correct for class 0.'
     assert met[1]['sensitivity_k1'] == 0.5, 'sens should be 0.5 when k=1, since first is incorrect.'
     assert np.isnan(met[2]['sensitivity_k1']), "Should be nan since no values predict class2"
