46eddb00610f76580edb90adc8cf848745228a77
Albert Jimenez <albertjimenezsanfiz@gmail.com>
2018-05-15 12:08:48 -0400

    update keras version, keras-model-specs

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-eval',
-    version='0.0.8',
+    version='0.0.9',
     description='A evaluation abstraction for Keras models.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
     url='https://www.triage.com/',
     packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
     install_requires=[
-        'Keras==2.1.4',
+        'Keras>=2.1.6',
         'h5py',
         'tensorflow',
         'Pillow',
@@ -21,6 +21,6 @@ setup(
         'plotly',
         'matplotlib',
         'sklearn',
-        'keras-model-specs>=0.0.24',
+        'keras-model-specs>=0.0.25',
     ]
 )
