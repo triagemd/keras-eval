787528c7365f07890765a6a15b080dca146dbb69
Adria Romero <adriaromero@me.com>
2018-08-21 11:58:32 -0400

    replace relu6 with ReLU layers

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-eval',
-    version='0.0.25',
+    version='0.0.26',
     description='An evaluation abstraction for Keras models.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
     url='https://www.triage.com/',
     packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
     install_requires=[
-        'Keras>=2.2.0',
+        'Keras>=2.2.2',
         'h5py',
         'Pillow',
         'scipy',
