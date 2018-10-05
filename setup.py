9d23f1a893b38070c01f0fc03a054b28e78b4798
Adria Romero <adriaromero@me.com>
2018-10-05 16:05:27 -0400

    bump version + update keras-model-specs min version

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-eval',
-    version='0.0.32',
+    version='0.0.33',
     description='An evaluation abstraction for Keras models.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
     url='https://www.triage.com/',
     packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
     install_requires=[
-        'Keras>=2.2.2',
+        'Keras>=2.2.0',
         'h5py',
         'Pillow',
         'scipy',
@@ -21,6 +21,6 @@ setup(
         'plotly',
         'matplotlib',
         'sklearn',
-        'keras-model-specs>=0.0.27',
+        'keras-model-specs>=0.0.28',
     ]
 )
