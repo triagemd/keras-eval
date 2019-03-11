a69e552c47aca7f78afbb7b4ec0fe45fd359b291
Albert Jimenez <albertjimenezsanfiz@gmail.com>
2019-03-11 13:52:52 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-eval',
-    version='1.0.4',
+    version='1.1.1',
     description='An evaluation abstraction for Keras models.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
