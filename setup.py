e989daa20abc3263c218cca0d122cd7ecafffe85
Adria Romero <adriaromero@me.com>
2019-04-17 14:52:02 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-eval',
-    version='1.1.2',
+    version='1.1.3',
     description='An evaluation abstraction for Keras models.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
